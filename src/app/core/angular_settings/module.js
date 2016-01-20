2ad19d4fbd46dd9c847774a9ffe727937aac1dfe
eespinetti <eespinetti@4geeks.com.ve>
2016-01-20 01:05:29 -0430

    Agregando boton download list

--- src/app/core/angular_settings/module.js
+++ src/app/core/angular_settings/module.js
@@ -3,6 +3,7 @@
 
   angular
     .module('motelNo', ['ngAnimate', 'ngResource', 'ui.router', 'ui.bootstrap', 'toastr', 'chart.js',
-                        'angular-momentjs','satellizer', 'angularUtils.directives.dirPagination']);
+                        'angular-momentjs','satellizer', 'angularUtils.directives.dirPagination',
+                        'frapontillo.bootstrap-switch']);
 
 })();
