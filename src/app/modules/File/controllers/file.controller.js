cf76abbec94ef71e583bdb96324b18499cd1758c
nestor <ngodoy@4geeks.com.ve>
2016-02-10 10:12:35 -0430

    tempo-upload

--- src/app/modules/File/controllers/file.controller.js
+++ src/app/modules/File/controllers/file.controller.js
@@ -6,16 +6,36 @@
 
   angular
     .module('motelNo')
-    .controller('FileController', function($scope, Upload, $timeout){
-
+    .controller('FileController', function($scope, Upload, $timeout , base_url){
+    console.log()
       var vm = this;
+
+      $scope.uploadPic = function(file) {
+        file.upload = Upload.upload({
+          url:  base_url+'Location',
+          data: {image: file, name: $scope.username},
+        });
+
+        file.upload.then(function (response) {
+          $timeout(function () {
+            file.result = response.data;
+          });
+        }, function (response) {
+          if (response.status > 0)
+            $scope.errorMsg = response.status + ': ' + response.data;
+        }, function (evt) {
+          // Math.min is to fix IE which reports 200% sometimes
+          file.progress = Math.min(100, parseInt(100.0 * evt.loaded / evt.total));
+        });
+      }
+
 
       $scope.uploadFiles = function(file, errFiles) {
         $scope.f = file;
         $scope.errFile = errFiles && errFiles[0];
         if (file) {
           file.upload = Upload.upload({
-            url: 'http://localhost:8080/motel-upload/upload.php',
+            url: base_url+'Location',
             method: 'POST',
             file: file,
             headers: {'Content-Type': undefined},
@@ -43,4 +63,7 @@
       };
 
     });
+
+
+
 })();
